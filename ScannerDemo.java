5d50ca05356603aa68eeb0dda58034f47b94b4af
Isa <isagunes0635@gmail.com>
2024-08-08 13:28:04 +0300

    Keyboard input demonsration in JAVA

---
+++ ScannerDemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of pods following by:");
+        System.out.println("the number of peas in a pod:");
+        int numberOfPods = scanner.nextInt();
+        int peasPerPods = scanner.nextInt();
+
+        int totalNumberOfPeas = numberOfPods * peasPerPods;
+
+        System.out.print(numberOfPods + " pods and ");
+        System.out.println(peasPerPods + " peas per pod.");
+        System.out.println("The total number of peas " + totalNumberOfPeas);
+    }
+}
